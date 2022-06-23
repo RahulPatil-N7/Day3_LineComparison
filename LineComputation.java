a1bb5b6fa82f068620d3516c04f254b85ee6c906
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-23 18:05:16 +0530

    Welcome to Line comparison program.

---
+++ LineComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.LineBranch;
+
+public class LineComputation {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the line comparison computation.");
+		
+	    
+	}
+} 
