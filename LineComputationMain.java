c1e4f6d5ac900678fefaf24cdaadb005f5c2d6c1
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-06-29 18:57:33 +0530

    Added :Welcome message.

---
+++ LineComputationMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.LineBranch;
+
+public class LineComputationMain {
+
+	public static void main(String[] args) {
+		//Printing welcome message
+		System.out.println("Welcome to Line Comparison Computation Java Program.");
+      }
+}
